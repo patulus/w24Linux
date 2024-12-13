27e3239c23c2aa94301b025f6fad99de9803512c
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-12-13 16:24:30 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210463/w24Linux/W24LinuxApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210463.w24Linux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24LinuxApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24LinuxApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210463/w24Linux/W24LinuxApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210463.w24Linux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24LinuxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
